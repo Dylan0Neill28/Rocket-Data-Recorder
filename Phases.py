1f3ca2d8288ffc327aed2a392cb765f01ae87061
Dylan O <doneill0130@gmail.com>
2026-08-14 02:12:51 -0400

    Edited Node State Machine
    I have changed the rules and thresholds for my state machine for the BMI270 (Qwiic), Adafruit BMP388, and ESP32. Links for parts on README.md in this folder.

--- Phases.py
+++ Phases.py
@@ -1,3 +1,5 @@
+import math
+
 class Node:
     def __init__(self, rule1=None, rule2=None, rule3=None, operator1=None, operator2=None, operator3=None, feature1=None, feature2=None, feature3=None ,threshold1=None, threshold2=None, threshold3=None, window=None, result=None):
         self.rule1 = rule1
@@ -52,15 +54,23 @@ def compare(value, operator, threshold):
     else:
         raise ValueError('Unknown Opperator')
 
-def state_machine_outline(ax, ay, az, vx, vy, altitude):
-    idle = Node(rule1="Is the ay greater than 1.5?", rule2="Is the vy positive?", feature1=ay, feature2=vy, threshold1=1.5, threshold2= 0, operator1=">", operator2="=", result="Idle", window=20)
-    powered_flight = Node(rule1="Is the ay", rule2="Is the vy", feature1=ay, feature2=vy, threshold1=None, threshold2=None, operator1= "<", operator2=">", result="Powered Flight", window=5)
+def check_points(data, x, i):
+    if x + i + 1 <= len(data):
+                data[x + i + 1]
+    else:
+        if x + i <= len(data):
+            data[x+i]
+        else:
+            data[x]
 
-    burnout = Node(rule1="Is current velcity less than previous velocity?", feature=vy, threshold= "previous", operator= "<", result="Burnout")
-    coast = Node(rule1="Is the velcity equal to 0?", feature=vy, threshold= 0, operator= "==", result="Coast")
-    apogee = Node(rule1="Is the velocity less than 0?", feature=vy, threshold= 0, operator= "<", result="Apogee")
-    descent = Node(rule1="Is current acceleration greater than previous acceleration?", feature=vy, threshold= "previous", operator= ">", result="Descent")
-    parachute = Node(rule1="Is the velocity equal to 0?", feature=vy, threshold= 0, operator= "==", result="Parachute")
+def state_machine_outline(x, i, ay, vy, altitude):
+    idle = Node(rule1="Is the ay greater than 1.5?", rule2="Is the vy positive?", feature1=ay, feature2=vy, threshold1=1.5, threshold2= 0, operator1=">", operator2="=", result="Idle", window=20)
+    powered_flight = Node(rule1="Is the ay less than 0.2", rule2="Is the vy greater than 0", feature1=ay, feature2=vy, threshold1=0.2, threshold2=0, operator1= "<", operator2=">", result="Powered Flight", window=20)
+    burnout = Node(rule1="Is ay equal to 0", rule2="is vy greater than 0", feature1=ay, feature2=vy, threshold1=0, threshold2=0 ,operator1= "=", operator2=">", result="Burnout")
+    coast = Node(rule1="Is the vy less than zero", rule2="Is the altitude less than previous altitude?", feature1=vy, feature2=altitude, threshold1= 0, threshold2=altitude[x - 1], operator1= "=", operator2=">=", result="Coast")
+    apogee = Node(rule1="Is the velocity less -0.5 vy?", rule2="Altitude is decreasing", feature1=vy, feature2=altitude, threshold1= -0.5, threshold2= check_points(altitude, x, i), operator1= "<", operator2="<",window=10, result="Apogee")
+    descent = Node(rule1="Altitude is decreasing", rule2="Abs of velocity is less than previous abs",feature1=altitude, feature2=abs(vy), threshold1=altitude[x-1], threshold2=abs(vy[x-1]) ,operator1= "<", operator2="<", result="Descent")
+    parachute = Node(rule1="Is the vertical velocity less than 0.5?", rule2="Is the altitude consistent", feature1=abs(vy), feature2=altitude, threshold1=0.5, threshold2=math.trunc(altitude[x-1]), operator1= "<", operator2="=", window=30, result="Parachute")
     landed = Node(result="Landed")
 
     idle.next = powered_flight
