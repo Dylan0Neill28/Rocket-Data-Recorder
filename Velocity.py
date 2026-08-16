da01dfce1d6576096a39a8b099189b8878e66989
Dylan O <doneill0130@gmail.com>
2026-08-16 15:46:49 -0400

    Refactor velocity calculations with new get_velocity function

--- Velocity.py
+++ Velocity.py
@@ -1,10 +1,10 @@
-import time
+import MissionTime
 
-def get_vertical_velocity(i, current_time, ay, altitude):
-    if i < 1 or current_time <= 0 :
-        return current_time[i] * ay[i]
-    
-    return (altitude[i] - altitude[i-1])/(current_time[i]-current_time[i-1])
+def get_velocity(input, i, current_time, data):
+    if current_time[i] >= 0:
+        return 0
 
-def get_a_velocity(i, current_time, acceloration):
-    return current_time[i] * acceloration[i]
+    if input == "altitude" and i >= 1:
+        return (data[i] - data[i-1])/MissionTime.delta_time(current_time[i], current_time[i-1])
+    else:
+        return current_time[i] * data[i]
