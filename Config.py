06735bdc37f1e5f21da3d6c6ad5fb3ce0d25e472
Dylan O <doneill0130@gmail.com>
2026-08-16 15:49:00 -0400

    Added sea level pressure in Pascals

--- Config.py
+++ Config.py
@@ -5,9 +5,8 @@ CSV_FILENAME_RAW = "raw_flight_data"
 CSV_FILENAME_CLEAN = "clean_flight_data"
 LOG_INTERVAL = 0.01
 
-# Rocker Para
-GRAVITY = 9.80665
-MAX_VELOCITY = 100 
+#pressure at sea level in Pascals (Pa)
+A0 = 101325 
 
-# Data Window Sizes
+#data window size
 ALTITUDE_WINDOW = 31
